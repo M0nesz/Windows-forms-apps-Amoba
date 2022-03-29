b8a362a9ebe00b49d66dcd7dbd32acc2a7fe0b18
M0nesz <balimn13@gmail.com>
2022-03-29 11:41:25 +0200

    Főmenü kész

--- TicTacToe/Form1.Designer.cs
+++ TicTacToe/Form1.Designer.cs
@@ -28,12 +28,129 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.scores = new System.Windows.Forms.Button();
+            this.credits = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1118, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-424, -44);
+            this.button1.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1063, 543);
+            this.button2.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(247, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 881);
+            this.label2.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 46);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(309, 211);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(160, 65);
+            this.start.TabIndex = 4;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.BackColor = System.Drawing.Color.Transparent;
+            this.Title.Font = new System.Drawing.Font("Eras Light ITC", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Title.Location = new System.Drawing.Point(95, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(608, 136);
+            this.Title.TabIndex = 5;
+            this.Title.Text = "Tic Tac Toe";
+            // 
+            // scores
+            // 
+            this.scores.Location = new System.Drawing.Point(309, 332);
+            this.scores.Name = "scores";
+            this.scores.Size = new System.Drawing.Size(160, 65);
+            this.scores.TabIndex = 6;
+            this.scores.Text = "Scores";
+            this.scores.UseVisualStyleBackColor = true;
+            // 
+            // credits
+            // 
+            this.credits.AutoSize = true;
+            this.credits.BackColor = System.Drawing.Color.Transparent;
+            this.credits.Font = new System.Drawing.Font("Eras Light ITC", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.credits.Location = new System.Drawing.Point(0, 524);
+            this.credits.Name = "credits";
+            this.credits.Size = new System.Drawing.Size(445, 32);
+            this.credits.TabIndex = 7;
+            this.credits.Text = "Created by: Varró Ágnes, Bali Mónika";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(21F, 46F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::TicTacToe.Properties.Resources.backround;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.credits);
+            this.Controls.Add(this.scores);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Eras Light ITC", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+        private Button start;
+        private Label Title;
+        private Button scores;
+        private Label credits;
     }
 }
